9ef67ee37225873f8d4fd6195365ca943c2acb2d
Simone MARAVIGNA <simone.maravigna@ui01.hpc.unipr.it>
2023-07-16 20:55:56 +0200

    hpc path config

--- utils/hyper_param.py
+++ utils/hyper_param.py
@@ -9,14 +9,14 @@ def parse_args():
                         help='input batch size for training (default: 4)')
     parser.add_argument('--lr', type=float, default=1e-2,
                         help='learning rate (default: 1e-2)')
-    parser.add_argument('--ds-path', type=str, default='./data/cityscapes',
-                        help='dataset path (default: ./data/cityscapes)')
+    parser.add_argument('--ds-path', type=str, default='/hpc/home/simone.maravigna/Project_DLEA/data/cityscapes', # in normal condition './data/cityscapes' in hpc '/hpc/home/simone.maravigna/Project_DLEA/data/cityscapes'
+                        help='dataset path (default: ./data/cityscapes)') 
     # checkpoints
     parser.add_argument('--resume', type=str, default=None,
                         help='put the path to resuming file if needed')
-    parser.add_argument('--save-weights', default='./weights',
+    parser.add_argument('--save-weights', default='/hpc/home/simone.maravigna/Project_DLEA/weights', # in normal condition './weights' in hpc '/hpc/home/simone.maravigna/Project_DLEA/weights'
                         help='Directory for saving checkpoint models (default: ./weights)')
     # eval
-    parser.add_argument('--output-folder', type=str, default='./output_data',
+    parser.add_argument('--output-folder', type=str, default='/hpc/home/simone.maravigna/Project_DLEA/output_data', # in normal condition './output_data' in hpc '/hpc/home/simone.maravigna/Project_DLEA/output_data'
                         help='put the path to save output (default: ./output_data)')
     return parser.parse_args()
